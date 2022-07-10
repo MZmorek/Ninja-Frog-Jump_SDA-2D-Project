fd4a7c2edc18ef5dcbfccb9e8d00ff8849a301fc
MZmorek <malgorzata.zmorek@gmail.com>
2022-07-10 15:39:00 +0200

    Camera Controller implementation

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+namespace FrogNinja.CameraControls
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] PlayerController playerCharacter;
+        private Vector3 myPosition;
+
+        private void Start()
+        {
+            myPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            if(playerCharacter.transform.position.y > myPosition.y)
+            {
+                myPosition.y = playerCharacter.transform.position.y;
+                transform.position = myPosition;
+            }
+            
+        }
+    }
+}
+
